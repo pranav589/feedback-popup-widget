51307d05e135b609e03e682e8985cefc49e801af
Pranav Bakale <pranav.bakale@gmail.com>
2024-09-20 14:37:08 +0530

    cleanup: remove unnecessary consoles

--- src/components/Widget.jsx
+++ src/components/Widget.jsx
@@ -62,8 +62,6 @@ function Widget({ projectId }) {
   }, [projectId]);
 
   const compareDomains = (projectUrl, currentUrl) => {
-    console.log("Project Url", projectUrl);
-    console.log("Current Url", currentUrl);
     try {
       // If projectUrl is relative, construct it with the current origin
       const fullProjectUrl = projectUrl.startsWith("http")
@@ -76,15 +74,10 @@ function Widget({ projectId }) {
       // Now we can safely create the project URL object
       const projectUrlObj = new URL(fullProjectUrl);
 
-      console.log("Project URL Object:", projectUrlObj);
-      console.log("Current URL Object:", currentUrlObj);
-
       // Compare only the domain (hostname), ignoring paths
       if (projectUrlObj.hostname === currentUrlObj.hostname) {
-        console.log("Domains match!");
         setIsSameDomain(true); // Domains match
       } else {
-        console.log("Domains do not match.");
         setIsSameDomain(false); // Domains do not match
       }
     } catch (error) {
